5fe1fe2f61b572714e2a43243feaba02578766c8
kalrifai <khalid.alrifai@alten.it>
2024-04-10 13:03:26 +0300

    Fixing Errors | Rewrite the code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,42 +1,26 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace TicTacToe
-{
-    public partial class WelcomeView : UserControl
+{ 
+    public partial class MainWindow : Window
     {
-        public class StartGameEventArgs : EventArgs
-        {
-            public bool IsVsComputer { get; set; }
-            public string Difficulty { get; set; }
-        }
-
-        public event EventHandler<StartGameEventArgs> StartGameClicked;
-
-        public WelcomeView()
+        public MainWindow()
         {
             InitializeComponent();
+            ShowWelcomeView();
         }
 
-        private void StartGame_Click(object sender, RoutedEventArgs e)
+        private void ShowWelcomeView()
         {
-            var args = new StartGameEventArgs
-            {
-                IsVsComputer = (bool)(FindName("Play Against the Computer") as RadioButton)?.IsChecked,
-                Difficulty = Difficulty.IsEnabled ? (Difficulty.SelectedItem as ComboBoxItem)?.Content.ToString() ?? "Easy" : "Easy"
-            };
-
-            StartGameClicked?.Invoke(this, args);
+            WelcomeView welcomeView = new WelcomeView();
+            welcomeView.StartGameClicked += WelcomeView_StartGameClicked;
+            MainContent.Content = welcomeView;
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void WelcomeView_StartGameClicked(object sender, System.EventArgs e)
         {
-            var radioButton = sender as RadioButton;
-            if (radioButton != null)
-            {
-                Difficulty.IsEnabled = radioButton.Content.ToString() == "Play Against the Computer";
-            }
+            GameView gameView = new GameView();
+            MainContent.Content = gameView;
         }
     }
 }
